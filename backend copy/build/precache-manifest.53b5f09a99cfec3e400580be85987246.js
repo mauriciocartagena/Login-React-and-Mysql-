10e9f3ccfec0566409d4b3c38ea074f98f4da8e8
Mauricio Cartagena <mc973220@gmail.com>
2020-03-16 22:17:15 -0400

    first Commit

---
+++ backend copy/build/precache-manifest.53b5f09a99cfec3e400580be85987246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d7d529f0f895b9f8f0e62cd2d2077d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a4e210f0fe5371ea857",
+    "url": "./static/css/main.e8ff79f5.chunk.css"
+  },
+  {
+    "revision": "8d0e4dec455db19b76e5",
+    "url": "./static/js/2.1845dbe9.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "./static/js/2.1845dbe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4e210f0fe5371ea857",
+    "url": "./static/js/main.ecdc1ac2.chunk.js"
+  },
+  {
+    "revision": "2e4071f51402e00bf9b3",
+    "url": "./static/js/runtime-main.590084d8.js"
+  }
+]);
